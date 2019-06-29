45a307ec6b9ca40e6e86bfe9a6456df40aeeb9db
aleksandr.kudriavtsev <aleksandr.kudriavtsev.firefly@gmail.com>
2019-06-29 12:57:39 +0300

    Added test routes

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-
-const app = express();
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
+// Connect Database
+
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
